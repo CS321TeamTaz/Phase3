594b0b813b1acb66a1554b5508b460e5a36c2f3d
Dane Grote <dane.a.grote@gmail.com>
2017-10-15 10:07:50 -0500

    Added core template for homepage. Also started mocking up undergraduate-program page (in dropdown menu under Academics)

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Global Variables
+var carouselIndex = 0;
+var timerId;
+
+window.onload = rotate_carousel(carouselIndex);
+
+function rotate_carousel(destination) {
+    var i;
+    var slides = document.getElementsByClassName("carousel_slide");
+    var controls = document.getElementsByClassName("carousel_indicator");
+
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+
+    for(i = 0; i < controls.length; i++) {
+        controls[i].style.backgroundColor = "grey";
+    }
+    
+    carouselIndex++;
+    if (carouselIndex > slides.length) {
+        carouselIndex = 1;
+    }
+    
+    slides[carouselIndex-1].style.display = "block";
+    controls[carouselIndex-1].style.backgroundColor = "rgb(155, 22, 29)";
+
+    clearTimeout(timerId);
+    timerId = setTimeout(rotate_carousel, 7000, carouselIndex);
+}
